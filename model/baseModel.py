4ddf41e4a208fb6a19f5f3ece6c3f81938a0fa48
huydo25 <huydo0825@gmail.com>
2018-06-28 11:01:20 +0700

    fix base model

--- model/baseModel.py
+++ model/baseModel.py
@@ -1,5 +1,5 @@
-import os
 import tensorflow as tf
+import os
 
 class Model(object):
     def __init__(self, config):
@@ -14,35 +14,35 @@ class Model(object):
         self.session.run(init)
     
     def add_train_op(self, method, lr_rate, loss, clip=-1):
-        _method = method.lower()
+        _m = method.lower()
         
         with tf.variable_scope("train_step"):
-            if _method == 'adam':
+            if _m == 'adam':
                 optimizer = tf.train.AdamOptimizer(lr_rate)
-            elif _method == 'adagrad':
+            elif _m == 'adagrad':
                 optimizer = tf.train.AdagradOptimizer(lr_rate)
-            elif _method == 'sgd':
+            elif _m == 'sgd':
                 optimizer = tf.train.GradientDescentOptimizer(lr_rate)
-            elif _method == 'rmsprop':
+            elif _m == 'rmsprop':
                 optimizer = tf.train.RMSPropOptimizer(lr_rate)
             else:
-                raise NotImplementedError("Unknown method {}".format(_method))
+                raise NotImplementedError("Unknown method {}".format(_m))
             
             if clip > 0 :
-                grad, vs = zip(*optimizer.compute_gradients(loss))
-                grad, gnorm = tf.clip_by_norm(grad, clip)
-                self.train_op = optimizer.apply_gradients(zip(grad, vs))
+                gd, vs = zip(*optimizer.compute_gradients(loss))
+                gd, gnorm = tf.clip_by_norm(gd, clip)
+                self.train_op = optimizer.apply_gradients(zip(gd, vs))
             else:
                 self.train_op = optimizer.minimize(loss)
         
     def initialize_session(self):
-        self.log.info("Initialize Tensorflow Session")
+        self.log.info("Initialize tf session")
         self.session = tf.Session()
         self.session.run(tf.global_variables_initializer())
         self.save = tf.train.Saver()
         
     def restore_session(self, dir_model):
-        self.log.info("Reload the latest trained model")
+        self.log.info("Reload the latest trained model...")
         self.save.restore(self.session, dir_model)
     
     def save_session(self):
@@ -50,36 +50,41 @@ class Model(object):
             os.makedirs(self.config.dir_model)
         self.save.save(self.session, self.config.dir_model)
     
+    def close_session(self):
+        self.session.close()
+
+    
     def add_summary(self):
         self.merged = tf.summary.merge_all()
         self.file_writer = tf.summary.FileWriter(self.config,dir_output, self.session.graph)
         
     def train(self,train, dev):
-        fixed_score = 0
+        record = 0
         num_epoch_no_imprv = 0
         self.add_summary()
         
         for epoch in range(self.config.num_epochs):
             self.log.info("Epoch {:} out of {:}".format(epoch + 1,
-                        self.config.nepochs))
+                        self.config.num_epochs))
             score = self.run_epoch(train, dev, epoch)
-            self.config.learning_rate *= self.config.lr_decay
+            self.config.lr_rate *= self.config.lr_decay
             
-            if score >= fixed_score:
+            if score >= record:
                 num_epoch_no_imprv = 0
                 self.save_session()
-                fixed_score = score
-                self.log.info("-- new best score")
+                record = score
+                self.log.info("New best score recorded...")
             else:
                 num_epoch_no_imprv += 1
                 if num_epoch_no_imprv >= self.config.num_epoch_no_imprv:
-                    self.log.info("- early stopping {} epochs without "\
-                            "improvement".format(nepoch_no_imprv))
+                    self.log.info("Early stopping {} epochs without "\
+                            "improvement".format(num_epoch_no_imprv))
                     break            
                     
     def evaluate(self, test):
-        self.log.info("Testing model over test set")
-        metrics  = self.run_evaluate(test)
+        self.log.info("Evaluating on test set")
+        metrics = self.run_evaluate(test)
         msg = " - ".join(["{} {:04.2f}".format(k, v)
                 for k, v in metrics.items()])
         self.log.info(msg)
+        
